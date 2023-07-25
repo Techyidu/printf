6d6131dca1cdec73ca609cc130c02cd28faa2bb4
Techyidu <techyidu@gmail.com>
2023-07-25 12:13:01 -0700

    get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -3,39 +3,42 @@
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * @i: Index of the current character in the format string
+ * @list: List of arguments
  *
  * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
+int precision;
 
-	if (format[curr_i] != '.')
-		return (precision);
+/* Check if the precision is specified */
+if (format[*i + 1] != '.') {
+return -1;
+}
 
-	precision = 0;
+/* Initialize the precision */
+precision = 0;
 
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
+/* Iterate over the characters after the '.' */
+for (*i += 2; format[*i] != '\0'; *i++) {
+/* Check if the character is a digit */
+if (is_digit(format[*i])) {
+/* Multiply the precision by 10 and add the digit */
+precision *= 10;
+precision += format[*i] - '0';
+}
+/* Check if the character is a '*' */
+else if (format[*i] == '*') {
+/* Get the precision from the list of arguments */
+precision = va_arg(list, int);
+break;
+}
+/* Otherwise, the precision is invalid */
+else {
+break;
+}
+}
 
-	*i = curr_i - 1;
-
-	return (precision);
+return precision;
 }
